ee481652c07ef4a5aa8a64c33db9ccfd3b302b4f
pmkasidis <86877762+pmkasidis@users.noreply.github.com>
2021-07-08 07:56:04 +0700

    Update Z-Scores-Table.R

--- Z-Scores-Table.R
+++ Z-Scores-Table.R
@@ -1,5 +1,5 @@
 x <- seq(0,3.49,0.01)
 zd <- round(pnorm(x),5)
-list_name <- list(seq(0,0.09,0.01),seq(0,0.34,0.1))
-Zscore <- matrix(zd,nrow = 10 ,ncol = 35 ,dimname = list_name);
-t(Zscore)
+list_name <- list(seq(0,0.09,0.01),seq(0,3.4,0.1))
+Zscores <- matrix(zd,nrow = 10 ,ncol = 35 ,dimname = list_name);
+t(Zscores)
